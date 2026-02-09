5d1c532e4fe91a824ed45c04bb5985bb7ab26d36
Cristian Manolache <cristianmanolache0@gmail.com>
2026-02-09 21:44:46 +0200

    Add style for CalendarApp

--- src/components/CalendarApp.jsx
+++ src/components/CalendarApp.jsx
@@ -3,6 +3,7 @@ const CalendarApp = () => {
   return (
     <div className="calendar-app">
       <div className="calendar">
+        <h1 className="heading">Calendar</h1>
         <div className="navigate-date">
           <h2 className="month">May</h2>
           <h2 className="year">2024</h2>
